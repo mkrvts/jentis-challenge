df360da7aa31b928279c50aae0cd79fff2e15ffa
Marina Kryvets <marina@Marinas-MacBook-Pro.local>
2023-12-26 12:06:05 +0100

    Remove commented blocks

--- app/city-weather.py
+++ app/city-weather.py
@@ -1,20 +1,3 @@
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def get_static_data():
-#     data = {
-#         'message': 'disco pickle',
-#         'status': 'success'
-#     }
-#     return jsonify(data)
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
-
-
 import requests
 from flask import Flask, jsonify
 
